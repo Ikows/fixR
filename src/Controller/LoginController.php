89ac38dd50cc25b33c3e268651bb06099aa1b6ba
Nicolas Coulaud <coulauddev@gmail.com>
2018-12-19 19:17:55 +0100

    authentication

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
@@ -16,11 +18,22 @@ class LoginController extends AbstractController
     }
 
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/login", name="login")
+     * @Route("/login", name="app_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/login.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
     }
 }
