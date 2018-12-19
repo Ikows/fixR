566a604b9739e5acc30424c8845d70f6fbcdf004
Manon <baillet.manon@gmail.com>
2018-12-19 19:20:27 +0100

    Message Flashù

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -27,7 +28,7 @@ class AccountController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      * @Route("/create/journalist", name="create_journalist")
      */
-    public function createJournalist(Request $request, ObjectManager $manager)
+    public function createJournalist(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
 
@@ -35,6 +36,9 @@ class AccountController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user -> setPassword($hash);
+
             $manager->persist($user);
             $manager->flush();
 
@@ -43,7 +47,7 @@ class AccountController extends AbstractController
                 "Votre compte a bien été créé"
             );
 
-            return $this->redirectToRoute("app_login");
+            return $this->redirectToRoute("homepage");
         }
 
 
