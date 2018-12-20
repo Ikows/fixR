722ef5650b2647243e75c258c05206e61cd797cc
Laura Lenik <Laura.Lenik78@gmail.com>
2018-12-20 12:46:48 +0100

    bug nav et boutons

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'log' => true, 'retour' => true]);
     }
 
     /**
